372f5d6032b5ea3d43aae8539e8f47f68aa566a8
atiyemogharnas <56082033+atiyemogharnas@users.noreply.github.com>
2020-06-29 23:14:57 +0430

    component-movie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Movie from "./components/Movie";
+const axios = require('axios').default;
+class App extends React.Component {
+  state = {
+    movie: {
+        backdrop_path: '',
+        production_companies:[],
+        genres:[]
+    }
+    
+};
+componentWillMount() {
+  this.getMovieDetails(117570)
 }
 
+getMovieDetails(id){
+  axios.get('https://api.themoviedb.org/3/movie/'+ id + '?api_key=afb96b9738f60f3725583b1784cfaa0e')
+      .then( (response) => {
+          this.setState({movie:response.data})
+          console.log(this.state.movie)
+      })
+      .catch( (error) => {
+          console.log(error);
+      })
+      .finally( () =>{
+      });
+}
+
+render() {
+  const {movie} = this.state;
+  return (
+    <div className="App" style={{ flex:1,
+        backgroundImage: `linear-gradient(to right, rgba(9, 66, 59, 0.5) 0%, rgba(9, 28, 37, 0.5) 100%),url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,
+    }}>
+        <Movie movie={movie}/>
+    </div>
+);
+}
+}
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default class Movie extends React.Component {
+    render() {
+      return (
+        <section>
+          Movie
+        </section>
+      )
+    }
+  }
+
+
+
+
