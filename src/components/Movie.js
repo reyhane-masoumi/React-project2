346f444af2eef6056bd730f7daa16ae67294e77f
atiyemogharnas <56082033+atiyemogharnas@users.noreply.github.com>
2020-06-29 23:45:29 +0430

    back-ok

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,21 @@
 import React from 'react'
-export default class Movie extends React.Component {
-    render() {
-      return (
-        <section>
-          Movie
-        </section>
-      )
-    }
-  }
+import Background from "./Background";
 
+const Movie = ({movie}) => {
+    const {
+        title, runtime, revenue,
+        overview, release_date, poster_path,vote_average,genres,production_companies,tagline
+    } = movie;
+
+      return (
+        <div className={"row"}>
+        <Background posterPath={poster_path}/>
+        </div>
+        
+        );
+    };
+
+export default Movie
 
 
 
